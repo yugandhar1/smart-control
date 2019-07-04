72ae0cbff244d1506cd0aefd13fa2760ed22bde1
yugandhar1 <52240363+yugandhar1@users.noreply.github.com>
2019-07-04 09:59:19 +0530

    Add files via upload

---
+++ SmartHomeControl.ino
@@ -0,0 +1,84 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "harish";
+const char* password = "harish23";
+
+#define ORG "iip4gk"
+#define DEVICE_TYPE "vizag"
+#define DEVICE_ID "1234"
+#define TOKEN "harish1234"
+String command;
+char server[] = ORG ".messaging.internetofthings.ibmcloud.com";
+char topic[] = "iot-2/cmd/home/fmt/String";
+char authMethod[] = "use-token-auth";
+char token[] = TOKEN;
+char clientId[] = "d:" ORG ":" DEVICE_TYPE ":" DEVICE_ID;
+//Serial.println(clientID);
+
+WiFiClient wifiClient;
+void callback(char* topic, byte* payload, unsigned int payloadLength);
+PubSubClient client(server, 1883, callback, wifiClient);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  pinMode(D1,OUTPUT);
+  wifiConnect();
+  mqttConnect();
+}
+
+void loop() {
+  if (!client.loop()) {
+    mqttConnect();
+  }
+delay(100);
+}
+
+void wifiConnect() {
+  Serial.print("Connecting to "); Serial.print(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("nWiFi connected, IP address: "); Serial.println(WiFi.localIP());
+}
+
+void mqttConnect() {
+  if (!client.connected()) {
+    Serial.print("Reconnecting MQTT client to "); Serial.println(server);
+    while (!client.connect(clientId, authMethod, token)) {
+      Serial.print(".");
+      delay(500);
+    }
+    initManagedDevice();
+    Serial.println();
+  }
+}
+void initManagedDevice() {
+  if (client.subscribe(topic)) {
+    Serial.println("subscribe to cmd OK");
+  } else {
+    Serial.println("subscribe to cmd FAILED");
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int payloadLength) {
+  Serial.print("callback invoked for topic: "); Serial.println(topic);
+
+  for (int i = 0; i < payloadLength; i++) {
+    //Serial.println((char)payload[i]);
+    command += (char)payload[i];
+  }
+Serial.println(command);
+if(command == "LIGHTON"){
+  digitalWrite(D1,HIGH);
+  Serial.println("Light is Switched ON");
+}
+else if(command == "LIGHTOFF"){
+  digitalWrite(D1,HIGH);
+  Serial.println("Light is Switched OFF");
+}
+command ="";
+}
